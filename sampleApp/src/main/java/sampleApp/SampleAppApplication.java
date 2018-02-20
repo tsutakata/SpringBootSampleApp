b988f1e12291b5f3b3803299639d68cf8eb0cb04
tsutakata <akt216@gmail.com>
2018-02-21 01:44:45 +0900

    test

--- sampleApp/src/main/java/sampleApp/SampleAppApplication.java
+++ sampleApp/src/main/java/sampleApp/SampleAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleAppApplication {
 
-    //
+    // test2
 	public static void main(String[] args) {
 		SpringApplication.run(SampleAppApplication.class, args);
 	}
